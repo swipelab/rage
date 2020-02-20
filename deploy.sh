aa82ac3994b8fd929c899bf4452c9cc60137f1c2
alex <alex@swipelab.co>
2020-02-20 14:02:51 +0000

    deployyyyy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-docker build -t swipelab/rage-ghost -f ./ghost/Dockerfile ./ghost
-docker push swipelab/rage-ghost
+docker build -t swipelab/rage-ghost:latest -t swipelab/rage-ghost:$SHA -f ./ghost/Dockerfile ./ghost
+
+docker push swipelab/rage-ghost:latest
+docker push swipelab/rage-ghost:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/ghost-deployment ghost=swipelab/rage-ghost:$SHA
