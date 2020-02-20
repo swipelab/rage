137ae236cf4c833bbc82168b8e2ce6e0161f28f0
alex <alex@swipelab.co>
2020-02-20 13:23:06 +0000

    asserted

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+docker build -t swipelab/rage-ghost -f ./ghost/Dockerfile ./ghost
+docker push swipelab/rage-ghost
---
+++ ghost.test/Truthfulness.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ghost.test
+{
+  public class Truthfulness
+  {
+    [Fact]
+    public void Agreeableness()
+    {
+      Assert.True(true);
+    }
+  }
+}
